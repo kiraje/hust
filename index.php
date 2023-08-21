6fb797a93562e1b683f6821414ee0d7468acd9b9
kiraje <97667303+kiraje@users.noreply.github.com>
2023-08-21 10:04:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<title>Home one || Hurst</title>
-		<meta name="description" content="Hurst – Furniture Store eCommerce HTML Template is a clean and elegant design – suitable for selling flower, cookery, accessories, fashion, high fashion, accessories, digital, kids, watches, jewelries, shoes, kids, furniture, sports….. It has a fully responsive width adjusts automatically to any screen size or resolution.">
+		<meta name="description" content="Hurst – Furniture Store .">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
